523d160cf79af014ca694120a0b0f66236d10534
devansh-dvj <devansh.dvj@gmail.com>
2020-04-30 14:17:04 +0530

    Added list feature.

--- springer.py
+++ springer.py
@@ -10,26 +10,40 @@ wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 max = len(sheet.col(4))
 
-if len(sys.argv) == 1:
-    x, y = 2, max
-elif len(sys.argv) == 2:
-    try:
+list = []
+
+try:
+    if len(sys.argv) == 1:
+        x, y = 2, max
+    elif sys.argv[1] == '-l':
+        if len(sys.argv) == 2:
+            print('Parameter missing')
+            exit(0)
+        for l in range(2, len(sys.argv)):
+            val = int(sys.argv[l])
+            if val > max-2 or val < 1:
+                print('Parameter out of bounds', val)
+                exit(0)
+            list.append(val+1)
+    elif len(sys.argv) == 2:
         x, y = int(sys.argv[1])+1, int(sys.argv[1])+2
-    except:
-        print('Enter only numbers as parameter')
-        exit(0)
-else:
-    try:
+    elif len(sys.argv) == 3:
         x, y = int(sys.argv[1])+1, int(sys.argv[2])+2
-    except:
-        print('Enter only numbers as parameter')
+    else:
+        print("'-l' is required as the first parameter for list")
         exit(0)
 
-if y > max or x < 2:
-    print('Parameter out of bounds')
+except:
+    print('Enter only numbers as parameter')
     exit(0)
 
-for i in range(x, y):
+if list == []:
+    if y > max or x < 2:
+        print('Parameter out of bounds')
+        exit(0)
+    list = range(x, y)
+
+for i in list:
     page = requests.get(sheet.cell_value(i,4))
     site = BeautifulSoup(page.content, 'html.parser')
     down_button = site.find_all('a', attrs={'title':'Download this book in PDF format'})
